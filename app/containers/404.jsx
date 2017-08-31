2b829b06537f07478dccd8890463eff1447328aa
codeingCold <1099762747@qq.com>
2017-08-31 18:17:02 +0800

    修改404页面

--- app/containers/404.jsx
+++ app/containers/404.jsx
@@ -1,14 +1,42 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-
+import { hashHistory, Link } from 'react-router'
 class NotFound extends React.Component {
     constructor(props, context) {
         super(props, context);
+        this.state = {
+            time: 5,
+        }
+        this.toHome = null;
+        this.changeTimer = null;
         this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.changeTime = this.changeTime.bind(this);
+    }
+
+    componentDidMount() {
+        this.toHome = setTimeout(function(){
+            hashHistory.push('/');
+        }, 5000);
+        
+        this.changeTimer = setInterval(()=> {
+            this.changeTime();
+        }, 1000);
+    }
+    componentWillUnmount(){
+        console.log('unmount');
+        clearInterval(this.changeTimer);
+        clearTimeout(this.toHome);
+    }
+    changeTime() {
+        this.setState(prevState => ({
+            time: prevState.time - 1,
+        }));
     }
     render() {
         return (
-            <h1>404 not found page</h1>
+            <div>
+                <h3 className="notFound">404 not found page {this.state.time}秒后回到<Link to="/" activeClassName="to-home">首页</Link></h3>
+            </div>
         )
     }
 }
