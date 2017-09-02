7393f4dd2edbf18676467e9e97cb4f2288b1354d
SendLessLove <1099762747@qq.com>
2017-09-02 18:44:45 +0800

    轮播图组件

--- app/containers/Home/index.jsx
+++ app/containers/Home/index.jsx
@@ -1,27 +1,30 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
-import HomeHeader from '../../components/HomeHeader/index.jsx'
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux'
+import HomeHeader from '../../components/HomeHeader/index.jsx'
+import Category from '../../components/Category/index.jsx'
+
 
 class Home extends React.Component {
-    constructor(props, context) {
-        super(props, context);
-        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
-    }
-    render() {
-        return (
-            <div>
-                <HomeHeader cityName={this.props.userinfo.cityName} />
-            </div>
-        )
-    }
+  constructor(props, context) {
+    super(props, context);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+  render() {
+    return (
+      <div>
+        <HomeHeader cityName={this.props.userinfo.cityName} />
+        <Category />
+      </div>
+    )
+  }
 }
 
 function mapStateToProps(state) {
-    return {
-        userinfo: state.userinfo
-    }
+  return {
+    userinfo: state.userinfo
+  }
 }
 
 export default connect(
